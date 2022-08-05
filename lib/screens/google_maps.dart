ccb95f10e220ca1f3b35220cbaaf8aa8c0093b59
satriawarn <satriawarn@gmail.com>
2022-08-05 14:43:32 +0700

    Update Address Name Using geolocation and geocoding to get address name from latitude longitude

--- lib/screens/google_maps.dart
+++ lib/screens/google_maps.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutterauth/utils/snack_bar.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:location/location.dart';
+import 'package:location/location.dart' as lokasi;
 
 class GoogleMapsScreen extends StatefulWidget {
   const GoogleMapsScreen({Key? key}) : super(key: key);
@@ -15,11 +18,12 @@ class _GoogleMapsState extends State<GoogleMapsScreen> {
   final Set<Marker> _markers = {};
   final LatLng _currentPosition = const LatLng(-7.797068, 110.370529);
   GoogleMapController? _controller;
-  Location currentLocation = Location();
+  lokasi.Location currentLocation = lokasi.Location();
+  String? _address;
 
   void getLocation() async {
     await currentLocation.getLocation();
-    currentLocation.onLocationChanged.listen((LocationData loc) {
+    currentLocation.onLocationChanged.listen((lokasi.LocationData loc) {
       _controller?.animateCamera(
         CameraUpdate.newCameraPosition(
           CameraPosition(
@@ -28,14 +32,41 @@ class _GoogleMapsState extends State<GoogleMapsScreen> {
           ),
         ),
       );
+      // getAddressLocation(loc.latitude!, loc.longitude!);
+      // openSnackBar(context, "Your address is $_address", Colors.blue);
       // setState(() {
+      //   getAddressLocation(loc.latitude!, loc.longitude!);
       //   _markers.add(
       //     Marker(
       //       markerId: const MarkerId("Your Location"),
       //       position: LatLng(loc.latitude ?? 0.0, loc.longitude ?? 0.0),
+      //       infoWindow: InfoWindow(
+      //         title: "Address",
+      //         snippet: _address,
+      //       ),
       //     ),
       //   );
       // });
+    });
+  }
+
+  Future<void> getAddressLocation(double lat, double long) async {
+    List<Placemark> placemarks = await placemarkFromCoordinates(lat, long);
+
+    Placemark placeMark = placemarks[0];
+    String? name = placeMark.name;
+    String? subLocality = placeMark.subLocality;
+    String? locality = placeMark.locality;
+    String? administrativeArea = placeMark.administrativeArea;
+    String? postalCode = placeMark.postalCode;
+    String? country = placeMark.country;
+    String? address =
+        "$name, $subLocality, $locality, $administrativeArea $postalCode, $country";
+
+    print("ikilho $name");
+
+    setState(() {
+      _address = address;
     });
   }
 
@@ -69,6 +100,7 @@ class _GoogleMapsState extends State<GoogleMapsScreen> {
           myLocationButtonEnabled: true,
           myLocationEnabled: true,
           onTap: (position) {
+            getAddressLocation(position.latitude, position.longitude);
             setState(
               () {
                 _markers.add(
@@ -77,6 +109,10 @@ class _GoogleMapsState extends State<GoogleMapsScreen> {
                         MarkerId("${position.latitude}, ${position.longitude}"),
                     icon: BitmapDescriptor.defaultMarker,
                     position: position,
+                    infoWindow: InfoWindow(
+                      title: "Address",
+                      snippet: _address,
+                    ),
                   ),
                 );
               },
